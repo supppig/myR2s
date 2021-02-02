7c1d65a29e2bde481df0e180a15a9842923904a2
supppig-dw <supppig-dw@163.com>
2021-02-02 14:51:35 +0800

    update

--- add/6-repack.sh
+++ add/6-repack.sh
@@ -7,7 +7,8 @@ for i in 16g 2g; do
     ./mbrConf/mbrFix \
         -if=./bin/targets/rockchip/armv8/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-squashfs-sysupgrade.img.gz \
         -of=fix.img.gz \
-        -conf=./mbrConf/${i}.json
+        -conf=./mbrConf/${i}.json \
+        -q
     md=$(md5sum fix.img.gz | cut -d' ' -f1)
     mv fix.img.gz ./supppig/sdcard-${i}-${md}.img.gz
 done
