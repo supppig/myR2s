52b220fb3f94e611c28fab1b1b813971669d0906
supppig-dw <supppig-dw@163.com>
2021-02-02 09:36:14 +0800

    update

--- 1-init.sh
+++ 1-init.sh
@@ -21,4 +21,5 @@ chmod 777 *.sh
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
+sleep 5s
 df -h
--- add/4-download.sh
+++ add/4-download.sh
@@ -1,9 +1,12 @@
 make defconfig
 
 for i in $(seq 5); do
+    echo "第i次下载"
     make download -j10
     [ $? = 0 ] && break
 done
 
 find dl -size -1024c -exec rm -f {} \;
-dh -h
+
+sleep 5s
+df -h
