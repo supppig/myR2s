10144867cdd3c8312e9599ef656957e499e9fd6e
supppig <supppig@163.com>
2021-02-03 23:22:16 +0800

    update

--- add/7-upload.sh
+++ add/7-upload.sh
@@ -1,6 +1,8 @@
 curl -fsSL git.io/file-transfer | sh
 ./transfer cow --block 2621440 -s -p 64 --no-progress ./supppig 2>&1 | tee cowtransfer.log
 echo "::warning file=Firmware-Cowtransfer.com::$(cat cowtransfer.log | grep https)"
+echo "奶牛快传: $(cat cowtransfer.log | grep https)" >>url.log
 
 ./transfer wet -s -p 16 --no-progress ./supppig 2>&1 | tee wetransfer.log
 echo "::warning file=Firmware-Wetransfer.com::$(cat wetransfer.log | grep https)"
+echo "WeTransfer: $(cat wetransfer.log | grep https)" >>url.log
---
+++ add/8-release.sh
@@ -0,0 +1,11 @@
+assets=()
+for asset in ./supppig/*; do
+    assets+=("-a" "$asset")
+done
+
+da=$(date '+%Y%m%d-%H%M%S')
+echo $da >x.txt
+echo >>x.txt
+cat url.log >>x.txt
+
+hub release create ${assets[@]} -F x.txt "$da"
