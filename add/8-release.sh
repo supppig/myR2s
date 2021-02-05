75f93fd90091c85fcf976a68f6552389f2a2604e
supppig <supppig@163.com>
2021-02-06 00:16:48 +0800

    update

--- add/8-release.sh
+++ add/8-release.sh
@@ -6,8 +6,8 @@ for asset in ./supppig/*; do
 done
 
 da=$(date '+%Y%m%d-%H%M%S')
-echo $da >x.txt
-echo >>x.txt
-cat url.log >>x.txt
+echo $da >./x.txt
+echo >>./x.txt
+cat url.log >>./x.txt
 
-hub release create ${assets[@]} -F x.txt "$da"
+hub release create ${assets[@]} -F ./x.txt "$da"
