9d66f8f1adf659f1b8054311115f04fbbe9e2cc2
supppig <supppig@163.com>
2021-02-05 18:17:06 +0800

    update

---
+++ add/wht.sh
@@ -0,0 +1,18 @@
+echo 1234545 >url.log
+
+mkdir supppig
+echo 123 >./supppig/1.txt
+echo 456 >./supppig/2.txt
+
+
+assets=()
+for asset in ./supppig/*; do
+    assets+=("-a" "$asset")
+done
+
+da=$(date '+%Y%m%d-%H%M%S')
+echo $da >x.txt
+echo >>x.txt
+cat url.log >>x.txt
+
+hub release create ${assets[@]} -F x.txt "$da"
